320982e15d380acae97cf163746c277731d3a091
squee72564 <rodriguez.al183@gmail.com>
2024-12-11 08:15:48 -0800

    Finish day 2 of AOC

---
+++ 2/part_1.py
@@ -0,0 +1,30 @@
+import sys
+
+input_file = "test.txt"
+
+if len(sys.argv) > 1:
+    input_file = sys.argv[1]
+
+def check_safe(levels):
+    deltas = [levels[i+1] - levels[i] for i in range(len(levels)-1)]
+    if  (all(x < 0 and x in range(-3,0) for x in deltas) or
+         all(x > 0 and x in range(1,4) for x in deltas)):
+        return True
+    else:
+        return False
+
+        
+safe_lines = 0
+
+with open(input_file) as file:
+    lines = file.readlines()
+    
+    for line in lines:
+        levels = [int(level) for level in line.strip().split()]
+
+        if check_safe(levels):
+            safe_lines += 1
+    
+print(f"\nsafe lines: {safe_lines}")
+
+
